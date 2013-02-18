aa13af50c9527f6a7219ac88823ceb657632f3dd
Gillis Van Ginderachter <supergillis@gmail.com>
2013-02-18 15:37:13 +0100

    Cleanup Base.

--- base.js
+++ base.js
@@ -9,28 +9,19 @@ define(['utils'], function(µ) {
 
   Base.create = function(base, values) {
     var instance = Object.create(base);
+    var properties = {};
 
-    µ.each(base.properties, function(name, defaultValue) {
-      (function(value) {
-        Object.defineProperty(instance, name, {
-          configurable: false,
-          enumerable: false,
-          get: function() {
-            return value;
-          },
-          set: function(newValue) {
-            this.model.persistence.emit({
-              event: 'set',
-              callback: function() {
-                value = newValue;
-              },
-              args: [this, name, newValue]
-            });
-          }
-        });
-      })(values.hasOwnProperty(name) ? values[name] : defaultValue);
+    // Find properties with values from values and default values
+    µ.mapped(base.properties, properties, function(name, defaultValue) {
+      return values.hasOwnProperty(name) ? values[name] : defaultValue;
     });
 
+    // Define properties with the corresponding values
+    µ.each(properties, function(name, value) {
+      instance.defineProperty(name, value);
+    });
+
+    // Define untracked property values
     µ.each(values, function(name, value) {
       if (!instance.hasOwnProperty(name))
         instance[name] = value;
@@ -42,6 +33,25 @@ define(['utils'], function(µ) {
     });
 
     return instance;
+  };
+
+  Base.prototype.defineProperty = function(name, value) {
+    Object.defineProperty(this, name, {
+      configurable: false,
+      enumerable: false,
+      get: function() {
+        return value;
+      },
+      set: function(newValue) {
+        this.model.persistence.emit({
+          event: 'set',
+          args: [this, name, newValue],
+          callback: function() {
+            value = newValue;
+          }
+        });
+      }
+    });
   };
 
   Base.prototype.destroy = function() {
