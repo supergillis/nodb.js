79fde4796206da8f524eac7574a80f48981d5f37
Gillis Van Ginderachter <supergillis@gmail.com>
2013-03-25 20:15:09 +0100

    Implemented commit and revert operations in Revision.

--- Revision.js
+++ Revision.js
@@ -24,8 +24,17 @@ define([
       deleted: {
         value: Collection.linkedList()
       },
-      values: {
-        value: new WeakMap()
+      reads: {
+        value: new Map()
+      },
+      readKeys: {
+        value: []
+      },
+      writes: {
+        value: new Map()
+      },
+      writeKeys: {
+        value: []
       }
     });
   };
@@ -33,7 +42,19 @@ define([
   Revision.prototype = Object.create(Collection.prototype);
   Revision.prototype.constructor = Revision;
 
-  var rootRevision = new Revision();
+  var rootRevision = Object.create(new Revision(), {
+    commit: {
+      value: function() {
+        throw 'CommitException: Cannot commit the root revision!';
+      }
+    },
+    revert: {
+      value: function() {
+        throw 'CommitException: Cannot revert the root revision!';
+      }
+    }
+  });
+
   var currentRevision = rootRevision;
 
   Object.defineProperties(Revision, {
@@ -79,7 +100,41 @@ define([
    * @since 1.0.0
    */
   Revision.prototype.commit = function() {
-    console.log('commit');
+    // Implementation of algorithm described in section 5.5
+
+    // Step 1 and 3 of the algorithm
+    for (var index in this.readKeys) {
+      var instance = this.readKeys[index];
+      var instanceValues = this.reads.get(instance);
+
+      // Step 1 of the algorithm
+      for (var key in instanceValues) {
+        if (this.parent.getValueFor(instance, key) !==
+            instanceValues[key])
+          throw 'CommitException: \'' + key + '\' is already committed '
+            + 'to the parent by another child revision!';
+      }
+
+      // Step 3 of the algorithm
+      if (!this.parent.reads.has(instance))
+        this.parent.reads.set(instance, instanceValues);
+      else
+        µ.copy(instanceValues, this.parent.reads.get(instance));
+    }
+
+    // Step 2 of the algorithm
+    for (var index in this.writeKeys) {
+      var instance = this.writeKeys[index];
+      var instanceValues = this.writes.get(instance);
+
+      if (!this.parent.writes.has(instance))
+        this.parent.writes.set(instance, instanceValues);
+      else
+        µ.copy(instanceValues, this.parent.writes.get(instance));
+    }
+
+    // Step 4 of the algorithm
+    this.revert();
   };
 
   /**
@@ -89,9 +144,14 @@ define([
    * @since 1.0.0
    */
   Revision.prototype.revert = function() {
-    this.created = new Collection.LinkedList();
-    this.deleted = new Collection.LinkedList();
-    this.values = new WeakMap();
+    this.created = Collection.linkedList();
+    this.deleted = Collection.linkedList();
+
+    this.reads = new Map();
+    this.readKeys = [];
+
+    this.writes = new Map();
+    this.writeKeys = [];
   };
 
   /**
@@ -136,50 +196,62 @@ define([
   };
 
   /**
-   * @method hasOwnValueFor
-   *
-   * @author Gillis Van Ginderachter
-   * @since 1.0.0
-   */
-  Revision.prototype.hasOwnValueFor = function(instance, key) {
-    if (!this.values.has(instance))
-      return false;
-
-    var instanceValues = this.values.get(instance);
-    return instanceValues.hasOwnProperty(key);
-  };
-
-  /**
-   * @method getOwnValueFor
-   *
-   * @author Gillis Van Ginderachter
-   * @since 1.0.0
-   */
-  Revision.prototype.getOwnValueFor = function(instance, key) {
-    if (this.hasOwnValueFor(instance, key)) {
-      var instanceValues = this.values.get(instance);
-      return instanceValues[key];
-    }
-
-    return undefined;
-  };
-
-  /**
    * @method getValueFor
    *
    * @author Gillis Van Ginderachter
    * @since 1.0.0
    */
   Revision.prototype.getValueFor = function(instance, key) {
-    if (this.hasOwnValueFor(instance, key)) {
-      var instanceValues = this.values.get(instance);
-      return instanceValues[key];
+    // Implementation of algorithm described in section 5.3
+
+    // Step 1 of the algorithm
+    var current = this;
+    var result = undefined;
+
+    while (current) {
+      // Step 2 of the algorithm
+      if (current.writes.has(instance)) {
+        var instanceValues = current.writes.get(instance);
+        if (instanceValues.hasOwnProperty(key)) {
+          result = instanceValues[key];
+          break;
+        }
+      }
+      // Step 3 of the algorithm
+      else if (current.reads.has(instance)) {
+        var instanceValues = current.reads.get(instance);
+        if (instanceValues.hasOwnProperty(key)) {
+          result = instanceValues[key];
+          break;
+        }
+      }
+
+      // Step 4 of the algorithm
+      current = current.parent;
     }
 
-    if (this.parent)
-      return this.parent.getValueFor(instance, key);
+    // Step 5 of the algorithm
+    if (current === this)
+      return result;
 
-    return undefined;
+    if (!this.reads.has(instance)) {
+      // Step 6 of the algorithm
+      var instanceValues = {}
+
+      // Step 7 of the algorithm
+      instanceValues[key] = result;
+
+      this.reads.set(instance, instanceValues);
+      this.readKeys.push(instance);
+    }
+    else {
+      // Step 6 and 7 of the algorithm
+      var instanceValues = this.reads.get(instance);
+      if (!instanceValues.hasOwnProperty(key))
+        instanceValues[key] = result;
+    }
+
+    return result;
   };
 
   /**
@@ -189,14 +261,22 @@ define([
    * @since 1.0.0
    */
   Revision.prototype.setValueFor = function(instance, key, value) {
-    if (this.values.has(instance)) {
-      var instanceValues = this.values.get(instance);
+    // Implementation of algorithm described in section 5.2
+
+    if (this.writes.has(instance)) {
+      // Step 1 and 2 of the algorithm
+      var instanceValues = this.writes.get(instance);
       instanceValues[key] = value;
     }
     else {
+      // Step 1 of the algorithm
       var instanceValues = {};
+
+      // Step 2 of the algorithm
       instanceValues[key] = value;
-      this.values.set(instance, instanceValues);
+
+      this.writes.set(instance, instanceValues);
+      this.writeKeys.push(instance);
     }
   };
 
